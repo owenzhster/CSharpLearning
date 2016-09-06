069305efd860e39e28e99c85017e1bae30f8f649
owenzhster <125898286@qq.com>
2016-09-06 18:03:06 +0800

    add

---
+++ CSharpLearningSolution/CSharpLearning/express/RExpressionSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpLearning.express
+{
+    class RExpressionSample:IOutText
+    {
+         
+        public void outText()
+        {
+            string input = "Hello Weiming, welcome to C#";
+            string pattern = "\\s+";
+            string rstr = Regex.Replace(input, pattern, "e");
+            Console.WriteLine("input:"+input);
+            Console.WriteLine("After replace:" + rstr);
+        }
+    }
+}
